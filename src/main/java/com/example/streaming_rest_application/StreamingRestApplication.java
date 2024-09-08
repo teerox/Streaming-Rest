cd317610e6d190acb6b99d1b7910a1be9ca21131
teerox44 <anthony@publiclyme.com>
2024-09-08 19:33:08 -0300

    intial commit

---
+++ src/main/java/com/example/streaming_rest_application/StreamingRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.streaming_rest_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/streaming_rest_application/StreamingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streaming_rest_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
